bd9d71235a16b2d25513e88fc3a78207039e9c7d
Alexander Biryukov <74318440+AlexFromNorth@users.noreply.github.com>
2023-10-04 23:43:49 +0600

    added nested components

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import { useState } from "react";
 import Search from "./components/search/search";
 import TicketWrapper from "./components/tickets/TicketWrapper";
+import TicketItem from "./components/tickets/TicketItem";
 
 function App() {
   return (
     <>
       <div className="pl-20 pr-20 pt-5 pb-5">
         <Search />
-        <TicketWrapper/>
+        <TicketWrapper>
+          <TicketItem/>
+        </TicketWrapper>
+        <TicketWrapper>
+          <TicketItem/>
+          <TicketItem/>
+        </TicketWrapper>
 
       </div>
     </>
--- src/components/tickets/TicketWrapper.tsx
+++ src/components/tickets/TicketWrapper.tsx
@@ -2,10 +2,10 @@ import { Box } from "@mui/material";
 import React from "react";
 import TicketItem from "./TicketItem";
 
-const TicketWrapper = () => {
+const TicketWrapper = ({ children }) => {
   return (
     <Box className="mt-20 grid grid-cols-12 gap-6" sx={{ boxShadow: "0px 0px 20px rgba(170, 170, 170, 0.5)", borderRadius: '15px', paddingBottom: '27px'}}>
-      <TicketItem/>
+      { children }
     </Box>
   );
 };
